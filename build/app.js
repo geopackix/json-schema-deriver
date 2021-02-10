0ce58007bb0f958e40d91f4f922174b91a5b11dd
Manuel Hart <manuel.hart@geokoord.com>
2021-02-10 06:21:52 +0100

    adapted promise handling

--- build/app.js
+++ build/app.js
@@ -16,35 +16,33 @@ exports.deriveSchema = void 0;
  */
 function getNewSchema(name = "JSON File") {
     return __awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => {
-            let newSchema = {
-                description: "Auto-Generated JSON Schema for " + name + " using json-schema-deriver (https://github.com/geopackix/json-schema-deriver)",
-                title: name,
-                additionalProperties: false,
-                $schema: "http://json-schema.org/draft-04/schema#",
-            };
-            resolve(newSchema);
-        });
+        let newSchema = {
+            description: "Auto-Generated JSON Schema for " +
+                name +
+                " using json-schema-deriver (https://github.com/geopackix/json-schema-deriver)",
+            title: name,
+            additionalProperties: false,
+            $schema: "http://json-schema.org/draft-04/schema#",
+        };
+        return newSchema;
     });
 }
 /**
  * This exported function creats a new schema body
  */
-function deriveSchema(inObj, name = 'JSON File') {
+function deriveSchema(inObj, name = "JSON File") {
     return __awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                let schemaHeader = yield getNewSchema(name);
-                let schemaDefinitions = { definitions: {} };
-                let schemaProperties = getObjectSchema(inObj, schemaDefinitions.definitions);
-                //Concat Schema Header and Properties
-                var schema = Object.assign({}, schemaHeader, schemaProperties, schemaDefinitions);
-                resolve(schema);
-            }
-            catch (error) {
-                reject(error);
-            }
-        }));
+        try {
+            let schemaHeader = yield getNewSchema(name);
+            let schemaDefinitions = { definitions: {} };
+            let schemaProperties = getObjectSchema(inObj, schemaDefinitions.definitions);
+            //Concat Schema Header and Properties
+            var schema = Object.assign({}, schemaHeader, schemaProperties, schemaDefinitions);
+            return schema;
+        }
+        catch (error) {
+            throw error;
+        }
     });
 }
 exports.deriveSchema = deriveSchema;
@@ -56,22 +54,22 @@ function getObjectSchema(inObject, schemaDefinitions, parent) {
     //Check Type of Object
     objectSchema.type = typeof inObject;
     if (parent) {
-        objectSchema.description = 'Description of ' + parent;
+        objectSchema.description = "Description of " + parent;
     }
-    if (objectSchema.type == 'object') {
+    if (objectSchema.type == "object") {
         //Check if inObject is an array.
         if (Array.isArray(inObject)) {
-            objectSchema.type = 'array';
+            objectSchema.type = "array";
         }
     }
     //In case of 'string' or 'number' extract example values.
-    if (objectSchema.type == 'string' || objectSchema.type == 'number') {
+    if (objectSchema.type == "string" || objectSchema.type == "number") {
         objectSchema.examples = [inObject];
     }
     //If type is 'object' or 'array' recusive call of function.
-    if (objectSchema.type == 'array') {
+    if (objectSchema.type == "array") {
         let firstItem = inObject[0];
-        if (typeof firstItem == 'object') {
+        if (typeof firstItem == "object") {
             //Object Item
             let itemDefinition = getObjectSchema(firstItem, schemaDefinitions);
             let reference = null;
@@ -83,23 +81,23 @@ function getObjectSchema(inObject, schemaDefinitions, parent) {
             }
             else {
                 //create definition
-                schemaDefinitions[parent + '-element'] = itemDefinition;
-                schemaDefinitions[parent + '-element']["additionalProperties"] = false;
+                schemaDefinitions[parent + "-element"] = itemDefinition;
+                schemaDefinitions[parent + "-element"]["additionalProperties"] = false;
                 //add reference
-                reference = '#/definitions/' + parent + '-element';
+                reference = "#/definitions/" + parent + "-element";
             }
             objectSchema.items = {
-                $ref: reference
+                $ref: reference,
             };
         }
         else {
             //Simple Item
             objectSchema.items = {
-                type: typeof firstItem
+                type: typeof firstItem,
             };
         }
     }
-    else if (objectSchema.type == 'object') {
+    else if (objectSchema.type == "object") {
         objectSchema.properties = {};
         for (let propertyName in inObject) {
             objectSchema.properties[propertyName] = getObjectSchema(inObject[propertyName], schemaDefinitions, propertyName);
@@ -120,12 +118,11 @@ function getDefinitionByItem(item, schemaDefinitions) {
         let comp = compareArrays(itemChildTypes, defItems);
         let comp2 = compareArrays(itemChildNames, defNames);
         if (comp && comp2) {
-            return '#/definitions/' + def;
+            return "#/definitions/" + def;
         }
     }
     return false;
 }
-;
 function getChildItemTypesByDefinition(item) {
     //extract child elements of object
     let items = [];
@@ -134,7 +131,6 @@ function getChildItemTypesByDefinition(item) {
     }
     return items;
 }
-;
 function getChildItemNames(item) {
     //extract child elements of object
     let itemNames = [];
@@ -143,7 +139,6 @@ function getChildItemNames(item) {
     }
     return itemNames;
 }
-;
 /**
  * Compares two Arrays and returns true if they are equal.
  */
--- src/app.ts
+++ src/app.ts
@@ -1,181 +1,179 @@
-
 //const schemaBuilder = ()=>{return deriveSchema};
 
 /**
  * This function creats a new schema body
  */
-async function getNewSchema(name:string = "JSON File"){
-  return new Promise((resolve, reject) => {
-    let newSchema = {
-      description: "Auto-Generated JSON Schema for "+ name + " using json-schema-deriver (https://github.com/geopackix/json-schema-deriver)",
-      title: name,
-      additionalProperties: false,
-      $schema: "http://json-schema.org/draft-04/schema#",
-    }
-    resolve(newSchema);
-  });
+async function getNewSchema(name: string = "JSON File") {
+  let newSchema = {
+    description:
+      "Auto-Generated JSON Schema for " +
+      name +
+      " using json-schema-deriver (https://github.com/geopackix/json-schema-deriver)",
+    title: name,
+    additionalProperties: false,
+    $schema: "http://json-schema.org/draft-04/schema#",
+  };
+  return newSchema;
 }
 
-
 /**
- * This exported function creats a new schema body 
+ * This exported function creats a new schema body
  */
-export async function deriveSchema(inObj, name:string = 'JSON File'){
-  return new Promise(async (resolve, reject) => {
-    try {
-      let schemaHeader = await getNewSchema(name);
+export async function deriveSchema(inObj, name: string = "JSON File") {
+  try {
+    let schemaHeader = await getNewSchema(name);
 
-      let schemaDefinitions = {definitions:{}}
+    let schemaDefinitions = { definitions: {} };
 
-      let schemaProperties = getObjectSchema(inObj, schemaDefinitions.definitions);
+    let schemaProperties = getObjectSchema(
+      inObj,
+      schemaDefinitions.definitions
+    );
 
-      //Concat Schema Header and Properties
-      var schema = Object.assign({}, schemaHeader, schemaProperties, schemaDefinitions);
-      
-      resolve(schema);
-    } catch (error) {
-      reject(error);
-    }
-    
-  });
+    //Concat Schema Header and Properties
+    var schema = Object.assign(
+      {},
+      schemaHeader,
+      schemaProperties,
+      schemaDefinitions
+    );
+    return schema;
+  } catch (error) {
+    throw error;
+  }
 }
 
 /**
  * This function creats property schema for the given inObject
  */
-function getObjectSchema(inObject, schemaDefinitions?:any, parent?:string){
- 
-  let objectSchema:any = {}
+function getObjectSchema(inObject, schemaDefinitions?: any, parent?: string) {
+  let objectSchema: any = {};
 
   //Check Type of Object
-  objectSchema.type =  typeof inObject;
+  objectSchema.type = typeof inObject;
 
-  if(parent){
-    objectSchema.description = 'Description of ' + parent;
+  if (parent) {
+    objectSchema.description = "Description of " + parent;
   }
-  
-  if(objectSchema.type == 'object'){
+
+  if (objectSchema.type == "object") {
     //Check if inObject is an array.
-    if(Array.isArray(inObject)){
-      objectSchema.type = 'array';
+    if (Array.isArray(inObject)) {
+      objectSchema.type = "array";
     }
   }
-  
-  //In case of 'string' or 'number' extract example values.
-  if(objectSchema.type == 'string' || objectSchema.type == 'number'){
-    objectSchema.examples = [inObject]
-  }
-  
-  //If type is 'object' or 'array' recusive call of function.
-  if(objectSchema.type == 'array'){
-    
-    let firstItem = inObject[0]
 
-    if(typeof firstItem == 'object'){
+  //In case of 'string' or 'number' extract example values.
+  if (objectSchema.type == "string" || objectSchema.type == "number") {
+    objectSchema.examples = [inObject];
+  }
+
+  //If type is 'object' or 'array' recusive call of function.
+  if (objectSchema.type == "array") {
+    let firstItem = inObject[0];
+
+    if (typeof firstItem == "object") {
       //Object Item
       let itemDefinition = getObjectSchema(firstItem, schemaDefinitions);
 
-      
-
       let reference = null;
 
-      let definitionName = getDefinitionByItem(itemDefinition.properties, schemaDefinitions);
+      let definitionName = getDefinitionByItem(
+        itemDefinition.properties,
+        schemaDefinitions
+      );
 
       //Find matching definition for array items
-      if(definitionName){
+      if (definitionName) {
         //add reference
         reference = definitionName;
-      }else{
+      } else {
         //create definition
-        schemaDefinitions[parent+'-element'] = itemDefinition;
-        schemaDefinitions[parent+'-element']["additionalProperties"] = false
+        schemaDefinitions[parent + "-element"] = itemDefinition;
+        schemaDefinitions[parent + "-element"]["additionalProperties"] = false;
 
         //add reference
-        reference = '#/definitions/'+parent+'-element'
+        reference = "#/definitions/" + parent + "-element";
       }
 
       objectSchema.items = {
-        $ref: reference
+        $ref: reference,
       };
-
-    }else{
+    } else {
       //Simple Item
       objectSchema.items = {
-        type: typeof firstItem
+        type: typeof firstItem,
       };
     }
-
-
-
-    
-    
-  }else if(objectSchema.type == 'object'){
+  } else if (objectSchema.type == "object") {
     objectSchema.properties = {};
-    for (let propertyName in inObject){
-      objectSchema.properties[propertyName] = getObjectSchema(inObject[propertyName], schemaDefinitions, propertyName);
+    for (let propertyName in inObject) {
+      objectSchema.properties[propertyName] = getObjectSchema(
+        inObject[propertyName],
+        schemaDefinitions,
+        propertyName
+      );
     }
   }
 
-  return objectSchema
+  return objectSchema;
 }
 
 /**
  * Check if schema definition by name is already available
  */
-function getDefinitionByItem(item:object, schemaDefinitions:object){
-
+function getDefinitionByItem(item: object, schemaDefinitions: object) {
   //extract child elements of object
   let itemChildTypes = getChildItemTypesByDefinition(item);
   let itemChildNames = getChildItemNames(item);
 
-  for(let def in schemaDefinitions){
-    let defItems = getChildItemTypesByDefinition(schemaDefinitions[def].properties);
+  for (let def in schemaDefinitions) {
+    let defItems = getChildItemTypesByDefinition(
+      schemaDefinitions[def].properties
+    );
     let defNames = getChildItemNames(schemaDefinitions[def].properties);
 
     let comp = compareArrays(itemChildTypes, defItems);
     let comp2 = compareArrays(itemChildNames, defNames);
-    
-    if(comp && comp2){
-      return '#/definitions/'+def;
+
+    if (comp && comp2) {
+      return "#/definitions/" + def;
     }
   }
   return false;
-};
+}
 
-
-function getChildItemTypesByDefinition(item:object){
+function getChildItemTypesByDefinition(item: object) {
   //extract child elements of object
   let items = [];
-  for(let c in item){
+  for (let c in item) {
     items.push(item[c].type);
   }
-  return items
-};
+  return items;
+}
 
-function getChildItemNames(item:object){
+function getChildItemNames(item: object) {
   //extract child elements of object
   let itemNames = [];
-  for(let c in item){
+  for (let c in item) {
     itemNames.push(c);
   }
-  return itemNames
-};
+  return itemNames;
+}
 
 /**
- * Compares two Arrays and returns true if they are equal.
+ * Compares two Arrays (by property names and types) and returns true if they are equal .
  */
-function compareArrays(A, B){
-  if(A.length == B.length){
-    for(let i = 0; i < A.length; i++){
-      if(A[i] != B[i]){
+function compareArrays(A, B) {
+  if (A.length == B.length) {
+    for (let i = 0; i < A.length; i++) {
+      if (A[i] != B[i]) {
         return false;
       }
     }
-  }else{
+  } else {
     return false;
   }
-  
+
   return true;
 }
-
-
